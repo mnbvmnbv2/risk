d76aead4b30f4951c7f10afd77446cbf1fd5bc14
Ferdinand Schumacher Ytteborg <32966421+mnbvmnbv2@users.noreply.github.com>
2021-09-24 11:25:16 +0200

    Only last country

--- js.js
+++ js.js
@@ -10,20 +10,20 @@ const projection = d3
 	.translate([width / 2, height / 2]);
 const pathGenerator = d3.geoPath().projection(projection);
 
-d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then((data) => {
-	const countries = topojson.feature(data, data.objects.countries);
+const data = JSON.parse(mapjson);
 
-	console.log(countries);
+const countries = topojson.feature(data, data.objects.countries);
 
-	const paths = svg.selectAll('path').data(countries.features);
-	paths
-		.enter()
-		.append('path')
-		.attr('d', (d) => pathGenerator(d));
+console.log(countries);
 
-	$('path').on('click', function () {
-		console.log(countries.features[$('path').index(this)].properties.name);
-	});
+const paths = svg.selectAll('path').data(countries.features);
+paths
+	.enter()
+	.append('path')
+	.attr('d', (d) => pathGenerator(d));
+
+$('path').on('click', function () {
+	console.log(countries.features[$('path').index(this)].properties.name);
 });
 
 //GAME
